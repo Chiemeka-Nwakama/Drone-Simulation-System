9c4750e350bb4a6cb0dfc403e26f7df2a2dbd7a3
mcgin374 <mcgin374@umn.edu>
2023-04-19 19:56:45 -0500

    first decorator implementation

--- libs/transit/src/Robot.cc
+++ libs/transit/src/Robot.cc
@@ -7,6 +7,12 @@ Robot::Robot(JsonObject &obj) : details(obj) {
   direction = {dir[0], dir[1], dir[2]};
   speed = obj["speed"];
   available = true;
+  wallet = new RobotWallet(this);
+}
+
+Robot::~Robot() {
+  // Delete dynamically allocated variables
+  delete wallet;
 }
 
 JsonObject Robot::GetDetails() const { return details; }
