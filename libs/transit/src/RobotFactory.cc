841772f3b282483a7bba2ea5b14ede102b76306c
ulric244 <ulric244@umn.edu>
2023-05-02 22:25:57 -0500

    working on drone movement and wallet integration

--- libs/transit/src/RobotFactory.cc
+++ libs/transit/src/RobotFactory.cc
@@ -4,6 +4,8 @@ IEntity* RobotFactory::CreateEntity(JsonObject& entity) {
   std::string type = entity["type"];
   if (type.compare("robot") == 0) {
     std::cout << "Robot Created" << std::endl;
+    Robot *test = new RobotWallet(new Robot(entity));
+    std::cout << "testing for segfault here?" << std::endl;
     return new RobotWallet(new Robot(entity));
   }
   return nullptr;
