2f98bf7a9fdfdb14e4bdf3011f2e3083c132477c
ulric244 <ulric244@umn.edu>
2023-05-03 16:49:34 -0500

    calculating trip costs

--- libs/transit/src/RobotFactory.cc
+++ libs/transit/src/RobotFactory.cc
@@ -3,7 +3,7 @@
 IEntity* RobotFactory::CreateEntity(JsonObject& entity) {
   std::string type = entity["type"];
   if (type.compare("robot") == 0) {
-    RobotWallet *rw = new RobotWallet(new Robot(entity))
+    RobotWallet *rw = new RobotWallet(new Robot(entity));
     std::cout << "Robot Created with starting funds of $" << rw->GetMoney() << std::endl;
     return rw;
   }
