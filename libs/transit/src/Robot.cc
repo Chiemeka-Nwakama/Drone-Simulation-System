f87de4b99e796673e14a0cf3107e2a57fe232855
ulric244 <ulric244@umn.edu>
2023-05-03 01:19:52 -0500

    reverting Drone.cc and Robot.cc to originals

--- libs/transit/src/Robot.cc
+++ libs/transit/src/Robot.cc
@@ -1,30 +1,3 @@
-// #include "Robot.h"
-
-// Robot::Robot(JsonObject &obj) : details(obj) {
-//   JsonArray pos(obj["position"]);
-//   position = {pos[0], pos[1], pos[2]};
-//   JsonArray dir(obj["direction"]);
-//   direction = {dir[0], dir[1], dir[2]};
-//   speed = obj["speed"];
-//   available = true;
-// }
-
-// JsonObject Robot::GetDetails() const { return details; }
-
-// void Robot::SetAvailability(bool choice) { available = choice; }
-
-// void Robot::Rotate(double angle) {
-//   Vector3 dirTmp = direction;
-//   direction.x = dirTmp.x * std::cos(angle) - dirTmp.z * std::sin(angle);
-//   direction.z = dirTmp.x * std::sin(angle) + dirTmp.z * std::cos(angle);
-// }
-
-// void Robot::Update(double dt, std::vector<IEntity*> scheduler) {
-//   toDestination = new AstarStrategy(position, destination, graph);
-//   toDestination->Move(this, dt);
-//   position = destination;
-// }
-
 #include "Robot.h"
 
 Robot::Robot(JsonObject &obj) : details(obj) {
@@ -49,4 +22,10 @@ void Robot::Rotate(double angle) {
   Vector3 dirTmp = direction;
   direction.x = dirTmp.x * std::cos(angle) - dirTmp.z * std::sin(angle);
   direction.z = dirTmp.x * std::sin(angle) + dirTmp.z * std::cos(angle);
+}
+
+void Robot::Update(double dt, std::vector<IEntity*> scheduler) {
+  // toDestination = new AstarStrategy(position, destination, graph);
+  // toDestination->Move(this, dt);
+  // position = destination;
 }
