33b41e0af8be5fc0e84131685e45fc6fdcda6fd7
ulric244 <ulric244@umn.edu>
2023-05-02 22:41:26 -0500

    determining segfault cause

--- libs/transit/src/RobotFactory.cc
+++ libs/transit/src/RobotFactory.cc
@@ -3,7 +3,7 @@
 IEntity* RobotFactory::CreateEntity(JsonObject& entity) {
   std::string type = entity["type"];
   if (type.compare("robot") == 0) {
-    std::cout << "Robot Created" << std::endl;
+    std::cout << "Robot Created right here whoohoo" << std::endl;
     Robot *test = new Robot(entity);
     std::cout << "testing for segfault here?" << std::endl;
     return new RobotWallet(new Robot(entity));
