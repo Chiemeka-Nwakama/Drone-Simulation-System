90e5b0576ace3311245d5d760911c54a7e93a9cc
ulric244 <ulric244@umn.edu>
2023-05-03 22:02:37 -0500

    nearest bank function

--- libs/transit/src/Bank.cc
+++ libs/transit/src/Bank.cc
@@ -7,7 +7,6 @@
 Bank::Bank(JsonObject& obj) : details(obj) {
   JsonArray pos(obj["position"]);
   position = {pos[0], pos[1], pos[2]};
-  bankPositions.push_back(position); //pushes banks position to the vector in ientity that holds bank positions
 }
 
 Bank::~Bank() {}
